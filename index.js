c4a7e9b32ba21c2159b1490469bfbf4ab09585b2
erwinkulasic <erwinkulasic@gmail.com>
2021-11-15 23:05:34 +0100

    minor fixes and removed plugin feature.

--- index.js
+++ index.js
@@ -1,71 +1,60 @@
-const http = require('http'),
-    https = require('https'),
-    urlPattern = require('url-pattern'),
-    queryStr = require('qs');
+const [http, https, urlPattern, queryStr] = [
+	require('http'),
+	require('https'),
+	require('url-pattern'),
+	require('qs')];
 
-
-var epyc = {},
-    routes = [],
-    plugins = [];
+let epyc = {}, routes = [];
 
 const server = (HttpState = false, listener, port = 0, options) =>
-    (HttpState ? https : http)
-        .createServer(options, listener)
-        .listen(port)
+	(HttpState ? https : http)
+		.createServer(options, listener)
+		.listen(port);
 
 const response = (res) => {
-    return {
-        send: value => res.end(value),
-        setHeader: (name, value) => res.setHeader(name, value),
-        status: code => res.writeHead(code),
-        json: value => {
-            res.setHeader("Content-Type", "application/json")
-            res.end(JSON.stringify(value))
-        },
-        html: value => {
-            res.setHeader("Content-Type", "text/html")
-            res.end(value)
-        }
-    }
+	return {
+		send: (value) => res.end(value),
+		setHeader: (name, value) => res.setHeader(name, value),
+		status: (code) => res.writeHead(code),
+		json: (value) => {
+			res.setHeader('Content-Type', 'application/json');
+			res.end(JSON.stringify(value));
+		},
+		html: (value) => {
+			res.setHeader('Content-Type', 'text/html');
+			res.end(value);
+		},
+	};
 };
 
-const GetRoute = value => {
-    const [url, qv] = value.split('?')
+const GetRoute = (value) => {
+	const [url, qv] = value.split('?');
 
-    for (let i = 0; i < routes.length; i++) {
-        const { route, action, method } = routes[i];
-        var params = new urlPattern(route).match(url);
+	for (let i = 0; i < routes.length; i++) {
+		var elem = routes[i];
+		const params = elem.route.match(url);
+		if (params !== null) {
+			elem.params = params;
+			elem.query = queryStr.parse(qv)
+			return elem;
+		}
+	}
 
-        if (params === null)
-            continue;
-
-        var query = queryStr.parse(qv);
-
-        return { route, action, method, params, query };
-    }
-
-    return undefined;
+	return undefined;
 };
 
+http.METHODS.forEach((method) => epyc[method.toLowerCase()] =
+	(route, action) => routes.push({ method, action, route: new urlPattern(route) }));
 
-http.METHODS.forEach(method => epyc[method.toLowerCase()] =
-    (route, action) => routes.push({ method, action, route }));
-
-epyc.use = func => (typeof func === "function") ?? plugins.push(func);
-
-epyc.bootstrap = (
-    port = 3000 || process.env.Port,
-    options = undefined,
-    error = (req, res) => res.json({ error: req.url + " not found." })) =>
-    (routes && Object.keys(routes).length !== 0) ? server(options !== undefined ? true : false, (req, res) => {
-        const route = GetRoute(req.url);
-        if (route) {
-            req.params = route.params;
-            req.query = route.query;
-            (plugins.length !== 0 ?? plugins.map(e => e).call(req, res));
-            ((route.method === req.method) ? route.action(req, response(res)) : error(req, response(res)));
-        }
-    }, port, options) : console.error("\x1B[31m" + JSON.stringify({ error: "no routes are defined." }));
+epyc.bootstrap = (port = (3000 || process.env.Port), options = undefined, error = (req, res) => res.json({ error: req.url + ' not found.' })) =>
+	(routes && Object.keys(routes).length !== 0) ? server(options !== undefined ? true : false, (req, res) => {
+		const route = GetRoute(req.url);
+		if (route) {
+			req.params = route.params;
+			req.query = route.query;
+			((route.method === req.method) ? route.action(req, response(res)) : error(req, response(res)));
+		}
+	}, port, options) : console.log("epyc: routes aren't defined.");
 
 
 module.exports = epyc;

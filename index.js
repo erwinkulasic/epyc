f29ef34df0b29757b1fa8c8936791335aa0e4095
erwinkulasic <erwinkulasic@gmail.com>
2022-01-03 21:35:27 +0100

    Major changes to code style and improved route handler for middelware

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Http          = require('http'),
       QueryStrings  = require('qs');
 
 const ErrorTemplate = (req, res) => 
-      res.json({ error: req.url + ' not found or a mistake happend.' });
+      res.json({ error: req.url + ' not found or something went wrong.' });
 
 let epyc        = {}, 
     routes      = [], 
@@ -17,18 +17,24 @@ const Server = (useHttps, port, options, handler) =>
 
 const ResponseFunctions = (response) => {
     return {
-        send: (value) => response.end(value),
-        status: (code) => response.writeHead(code),
-        setHeader: (name, value) => response.setHeader(name, value),
+        send(value) { 
+            response.end(value) 
+        },
+        status(code) {
+             response.writeHead(code)
+        },
+        setHeader(name, value) {
+            response.setHeader(name, value)
+        },
         json(value) {
-            response.setHeader('Content-Type', 'application/json');
-            response.end(JSON.stringify(value));
+            response.setHeader('Content-Type', 'application/json')
+            response.end(JSON.stringify(value))
         },
         html(value) {
-            response.setHeader('Content-Type', 'text/html');
-            response.end(value);
-        },
-    };
+            response.setHeader('Content-Type', 'text/html')
+            response.end(value)
+        }
+    }
 };
 
 const LookupRoute = (url) => {
@@ -39,34 +45,40 @@ const LookupRoute = (url) => {
     return [undefined, undefined];
 };
 
-const RouteHandler = (req, res, error) => {
-    const [url, query] = req.url.split('?');
-    const [params, route] = LookupRoute(url);
-    let index = 0;
 
+const RouteHandler = (req, res, error) => {
+    let index               = 0;
+    const [url, query]      = req.url.split('?');
+    const [params, route]   = LookupRoute(url);
+    
     if (params === undefined || route === undefined || req.method !== route.method) {
         error(req, ResponseFunctions(res)); return;
     }
 
-    req.params = params;
-    req.query = QueryStrings.parse(query);
+    req.params  = params;
+    req.query   = QueryStrings.parse(query);
 
-    const Activator = (req, res) => plugins[index](req, res, () => { 
-        index++; 
+    const Handle = () => route.middelware
+        ? route.middelware(req, res, () => route.handler(req, ResponseFunctions(res))) 
+        : route.handler(req, ResponseFunctions(res));
+    
+    const Activator = () => plugins[index](req, res, () => 
+        ++index < plugins.length 
+        ? Activator() 
+        : Handle())
 
-        if (index < plugins.length) Activator(req, res)
-        else route.handler(req, ResponseFunctions(res));
-    })
-
-    plugins.length !== 0 ? Activator(req, res) : route.handler(req, ResponseFunctions(res));
+    plugins.length !== 0 
+        ? Activator() 
+        : Handle();
 };
 
 for (let method of Http.METHODS) {
-    epyc[method.toLowerCase()] = (route, handler) => 
+    epyc[method.toLowerCase()] = (route, handler, handler1 = undefined) => 
         routes.push({ 
             method, 
-            handler, 
-            route: new UrlPattern(route) 
+            handler:    handler1 !== undefined ? handler1 : handler, 
+            route:      new UrlPattern(route),
+            middelware: handler1 !== undefined ? handler : undefined
         });
 }
 

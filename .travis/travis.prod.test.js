63931bad68242ffcb7525f03ce915399f564e385
erwinkulasic <erwinkulasic@gmail.com>
2022-06-12 18:04:30 +0200

    minor changes to travis test file.

--- .travis/travis.prod.test.js
+++ .travis/travis.prod.test.js
@@ -1,39 +1,67 @@
+/**
+ *  TRAVIS CI TEST
+ *   ---------------
+ *  This test is runned on travis CI.
+ *  It is used to test the production server.
+ */
 
-console.log("Initalize epyc for testing");
-const epyc = require('../index');
+'use strict';
+
+message("environment", process.env.NODE_ENV || 'development');
+
+//initialize the test environment
+const epyc = require('../src/index');
 const http = require('http');
-const util = require('util');
+const assert = require('assert');
 
-console.log("define route for testing");
+//add some data
 const obj = { Hello: "World" };
+console.table(obj);
+
+//add route
 epyc.get('/', (req, res) => res.json(obj));
 
-console.log("start the web server");
-const server = epyc.bootstrap(8080);
+//create server
+const PORT = process.env.PORT || 3000;
 
+const server = epyc.bootstrap(PORT, {
+    listen: () => {
+        message("server", `listening on port ${PORT}`)
 
-console.log("fetch our route");
-http.get("http://localhost:8080/", (resp) => {
-    let data = '';
-
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
-
-
-    resp.on('end', () => {
-        let obj1 = JSON.parse(data);
-        console.log(`response ${data}`);
-        console.log("response is equale to our route object: " + util.isDeepStrictEqual(obj1, obj))
-        console.log("Node application is running without any problems.");
-        server.close();
-    });
-
-}).on("error", (err) => {
-    console.log("Error: " + err.message);
-    server.close();
+        fetch(`http://localhost:${PORT}/`, (data) => {
+            try {
+                assert.deepStrictEqual(JSON.parse(data), obj);
+            } catch (err) {
+                message("error", err.message);
+            } finally {
+                message("server", "closed");
+                server.close();
+            }
+        });
+    }
 });
 
 
-//process.kill(0); cause a error
+function message(name, msg) {
+    console.log(`[\x1b[35mEPYC_TEST\x1b[0m] \x1b[32m${name} \x1b[0m: ${msg}`); 
+    //making a little bit more readable and colorful :)
+}
 
+function fetch(url, callback) {
+    http.get(url, (resp) => {
+        let data = '';
+
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            callback(data);
+        });
+
+    }).on("error", (err) => {
+        message("error", err.message);
+        message("server", "closed");
+        server.close();
+    });
+}
